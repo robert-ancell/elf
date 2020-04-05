28e5aa4d57ac81e35756f78f93a26c3b7e5ab7fb
Robert Ancell <robert.ancell@gmail.com>
2020-04-05 20:42:48 +1200

    Parse more syntax

--- elf.c
+++ elf.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +12,16 @@
 typedef enum {
     TOKEN_TYPE_WORD,
     TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_TEXT,
+    TOKEN_TYPE_ASSIGN,
+    TOKEN_TYPE_ADD,
+    TOKEN_TYPE_SUBTRACT,
+    TOKEN_TYPE_MULTIPLY,
+    TOKEN_TYPE_DIVIDE,
+    TOKEN_TYPE_OPEN_PAREN,
+    TOKEN_TYPE_CLOSE_PAREN,
+    TOKEN_TYPE_OPEN_BRACE,
+    TOKEN_TYPE_CLOSE_BRACE,
 } TokenType;
 
 typedef struct {
@@ -19,15 +30,76 @@ typedef struct {
     size_t length;
 } Token;
 
+static bool
+is_number_char (char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+static char
+ending_char_is_escaped (const char *data, Token *token)
+{
+    if (token->length < 3)
+        return false;
+
+    return data[token->offset + token->length - 2] == '\\';
+}
+
+static char
+string_is_complete (const char *data, Token *token)
+{
+    // Need at least an open and closing quote
+    if (token->length < 2)
+        return false;
+
+    // Both start and end characters need to be the same
+    char start_char = data[token->offset];
+    char end_char = data[token->offset + token->length - 1];
+    if (end_char != start_char)
+        return false;
+
+    // Closing quote needs to be non-escaped
+    return !ending_char_is_escaped (data, token);
+}
+
+static bool
+token_is_complete (const char *data, Token *token, char next_c)
+{
+   switch (token->type) {
+   case TOKEN_TYPE_WORD:
+       return isspace (next_c);
+   case TOKEN_TYPE_NUMBER:
+       return !is_number_char (next_c);
+   case TOKEN_TYPE_TEXT:
+       return string_is_complete (data, token);
+   case TOKEN_TYPE_ASSIGN:
+   case TOKEN_TYPE_ADD:
+   case TOKEN_TYPE_SUBTRACT:
+   case TOKEN_TYPE_MULTIPLY:
+   case TOKEN_TYPE_DIVIDE:
+   case TOKEN_TYPE_OPEN_PAREN:
+   case TOKEN_TYPE_CLOSE_PAREN:
+   case TOKEN_TYPE_OPEN_BRACE:
+   case TOKEN_TYPE_CLOSE_BRACE:
+       return true;
+   }
+
+   return false;
+}
+
 static int
 parse_elf_source (const char *data, size_t data_length)
 {
     Token **tokens = NULL;
     size_t tokens_length = 0;
     Token *current_token = NULL;
+
     for (size_t offset = 0; offset < data_length; offset++) {
         // FIXME: Support UTF-8
         char c = data[offset];
+
+        if (current_token != NULL && token_is_complete (data, current_token, c))
+            current_token = NULL;
 
         if (current_token == NULL) {
             // Skip whitespace
@@ -36,18 +108,40 @@ parse_elf_source (const char *data, size_t data_length)
 
             tokens_length++;
             tokens = realloc (tokens, sizeof (Token *) * tokens_length); // FIXME: Double size each time
-            tokens[tokens_length - 1] = current_token = malloc (sizeof (Token));
-            memset (current_token, 0, sizeof (Token));
-            current_token->type = TOKEN_TYPE_WORD;
-            current_token->offset = offset;
-            current_token->length = 1;
+            Token *token = tokens[tokens_length - 1] = malloc (sizeof (Token));
+            memset (token, 0, sizeof (Token));
+            token->type = TOKEN_TYPE_WORD;
+            token->offset = offset;
+            token->length = 1;
+
+            if (c == '(')
+                token->type = TOKEN_TYPE_OPEN_PAREN;
+            else if (c == ')')
+                token->type = TOKEN_TYPE_CLOSE_PAREN;
+            else if (c == '{')
+                token->type = TOKEN_TYPE_OPEN_BRACE;
+            else if (c == '}')
+                token->type = TOKEN_TYPE_CLOSE_BRACE;
+            else if (c == '=')
+                token->type = TOKEN_TYPE_ASSIGN;
+            else if (c == '+')
+                token->type = TOKEN_TYPE_ADD;
+            else if (c == '-')
+                token->type = TOKEN_TYPE_SUBTRACT;
+            else if (c == '*')
+                token->type = TOKEN_TYPE_MULTIPLY;
+            else if (c == '/')
+                token->type = TOKEN_TYPE_DIVIDE;
+            else if (is_number_char (c))
+                token->type = TOKEN_TYPE_NUMBER;
+            else if (c == '"' || c == '\'')
+                token->type = TOKEN_TYPE_TEXT;
+            else
+                token->type = TOKEN_TYPE_WORD;
+
+            current_token = token;
         }
         else {
-            if (isspace (c)) {
-                current_token = NULL;
-                continue;
-            }
-
             current_token->length++;
         }
     }
